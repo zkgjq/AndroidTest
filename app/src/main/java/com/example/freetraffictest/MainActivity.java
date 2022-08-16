8fc3e7e7f27abf28747061b30444e640f27bc117
zhangke11 <zhangke11@kuaishou.com>
2022-08-16 19:57:26 +0800

    改动弹窗文本

--- app/src/main/java/com/example/freetraffictest/MainActivity.java
+++ app/src/main/java/com/example/freetraffictest/MainActivity.java
@@ -11,7 +11,7 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        Toast.makeText(this, "测试弹窗!!!!", Toast.LENGTH_SHORT).show();
+        Toast.makeText(this, "测", Toast.LENGTH_SHORT).show();
         Toast.makeText(this, "测试弹窗2！", Toast.LENGTH_SHORT).show();
     }
 }
