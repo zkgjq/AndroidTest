7d75d1aa7c026fd9f0270363479f6d6c29a74d0d
zhangke11 <zhangke11@kuaishou.com>
2022-08-16 19:53:30 +0800

    改动弹窗1文本

--- app/src/main/java/com/example/freetraffictest/MainActivity.java
+++ app/src/main/java/com/example/freetraffictest/MainActivity.java
@@ -11,6 +11,6 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        Toast.makeText(this, "测试弹窗!!!!", Toast.LENGTH_SHORT).show();
+        Toast.makeText(this, "测试弹窗1-dasdsadasd", Toast.LENGTH_SHORT).show();
     }
 }
