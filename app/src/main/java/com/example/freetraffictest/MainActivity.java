9abbf4536d9199c20db4fc295792960d30b40ceb
zhangke11 <zhangke11@kuaishou.com>
2022-08-16 20:15:43 +0800

    删除一行

--- app/src/main/java/com/example/freetraffictest/MainActivity.java
+++ app/src/main/java/com/example/freetraffictest/MainActivity.java
@@ -12,7 +12,6 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         Toast.makeText(this, "测试弹窗1", Toast.LENGTH_SHORT).show();
-        Toast.makeText(this, "测试弹窗2", Toast.LENGTH_SHORT).show();
         Toast.makeText(this, "测试弹窗3", Toast.LENGTH_SHORT).show();
     }
 }
